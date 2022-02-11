e4e132e92d009bc354ccc22aef1e3f8e4edbf215
Sahad <iamsahadp@gmail.com>
2022-02-11 10:39:45 +0530

    Updates

---
+++ precache-manifest.578f01ae95531eacc3de43852e47ae0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c5cce982db24094866201b5437ec49",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "596caa48391892d8294d",
+    "url": "/notes/static/css/main.187e5434.chunk.css"
+  },
+  {
+    "revision": "f611e131d2aa17be3ac7",
+    "url": "/notes/static/js/2.1eed8f27.chunk.js"
+  },
+  {
+    "revision": "596caa48391892d8294d",
+    "url": "/notes/static/js/main.15acab3d.chunk.js"
+  },
+  {
+    "revision": "01155016b6fc9fe91e5f",
+    "url": "/notes/static/js/runtime~main.21ba3d73.js"
+  }
+]);
